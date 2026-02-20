91afc526a2bd6befe3b0094057166363e3171b6e
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-02-19 17:38:37 -0800

    Add Header component with navigation links

---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,27 @@
+//src/components/Header.jsx 
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/components.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+        <nav className="nav">
+            <ul className="nav-list">
+                <li className="nav-item">
+                    <Link to="/" className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/about" className="nav-link">About</Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/contact" className="nav-link">Contact</Link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  );
+};
+
+export default Header;
