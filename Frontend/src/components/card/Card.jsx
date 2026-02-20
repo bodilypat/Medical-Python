968c5bac8c53b94a897fa27f90662f32c0b2fb72
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-02-19 17:57:50 -0800

    Add Card component for displaying information

---
+++ Frontend/src/components/card/Card.jsx
@@ -0,0 +1,16 @@
+//src/components/card/Card.jsx 
+
+import React from 'react';
+import './card.css';
+
+function Card({ title, value, icon, bgColor }) {
+  return (
+    <div className="card" style={{ backgroundColor: bgColor }}>
+      <div className="card-icon">{icon}</div>
+      <h3 className="card-title">{title}</h3>
+      <p className="card-value">{value}</p>
+    </div>
+  );
+};
+
+export default Card;
