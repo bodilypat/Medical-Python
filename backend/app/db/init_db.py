c6595ae0edd873fb58f1abb47b74923122ec6c47
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-02-20 17:25:11 -0800

    Delete backend directory

--- backend/app/db/init_db.py
+++
@@ -1,8 +0,0 @@
-#app/db/init_db.py
-
-from app.db.session import engine 
-from app.db.base import Base 
-from app.models import patient, doctor, appointment
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
