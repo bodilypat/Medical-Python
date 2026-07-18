cd65fdc97b46cbc48f384679e4337d61ae078ecb
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-07-18 15:33:38 -0700

    Add files via upload

--- Backend/app/models/base.py
+++ Backend/app/models/base.py
@@ -1,15 +1,30 @@
-#app/models/base.py
+#File: app/models/base.py:
 
-from tortoise import fields, models 
-from tortorise.models import Model
-from enum import Enum
+from datetime import datetime
+from uuid import uuid4 
 
-class RoleEnum(str, Enum):
-    ADMIN = "admin"
-    USER = "user"
-    DOCTOR = "doctor"
-    PATIENT = "patient"
-    NURSE = "nurse"
+from sqlalchemy import Datetime 
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column 
 
-    
-    
+class Base(DeclarativeBase):
+    pass 
+
+class TimestampMaxin:
+    created_at: Mapped[datetime] = mapped_column(
+        Datetime,
+        defaultt=datetime.utcnow,
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow,
+    )
+
+class UUIDMixin:
+    id: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid4,
+    )
