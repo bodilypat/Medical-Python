a7931b9043924bee53d0c51ce55a3f84d286fdd0
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-07-22 14:42:36 -0700

    Add files via upload

--- Frontend/src/features/patients/services/patient.service.js
+++ Frontend/src/features/patients/services/patient.service.js
@@ -1,1 +1,70 @@
-File: src/features/patients/services/patient.service.js
+/* ******************************************************* */
+/* File: src/features/patients/services/patient.service.js */ 
+/* ******************************************************* */
+
+import api from "../../../services/axios";
+ 
+// Get all patients 
+// Backend APIRouter: GET / api/v1/patients
+
+export const getPatients = (params = {}) => {
+    return api.get("/patients", {
+        params, 
+    });
+};
+
+// Get patient by ID 
+//Backend APIRouter: GET/api/v1/patients/: id 
+
+export const getPatientById = (id) => {
+    return api.get(`/patients/${id}`);
+}
+
+//Create a new patient
+//Backend APIRouter -> POST / api/v1/patients
+
+export const createPatient = (patientData) => {
+    return api.post("/patients", patientData);
+};
+
+// Update an existing patient 
+// Backend APIRouter -> PUT /api/v1/patients/: id 
+export const updatePatient = (id, patientData) => {
+    return api.put(`/patients/${id}`, patientData);
+};
+
+// Delete a patient 
+// Backend APIRouter -> DELETE /api/v1/patient/:id 
+export const deletePatient = (id) => {
+    return api.delete(`/patients/${id}`);
+};
+
+// Search patients 
+// Backend APIRouter -> GET / api/v1/patients/search 
+export const searchPatient = (keyword) => {
+    return api.get("/patients/search", {
+        params: {
+            q: keyword,
+        },
+    });
+};
+
+// Upload patient document 
+// Backend APIRouter -> POST /api/v1/patients/:id/upload 
+
+export const uploadPatientFile = (id, file) => {
+    const formData = new FormData();
+
+    formData.append("file", file);
+
+    return api.post(
+        `/patients/${id}/upload`,
+        formData,
+        {
+            headers: {
+                "Content-Type" : "multipart/form-data",
+            },
+        }
+    );
+};
+
