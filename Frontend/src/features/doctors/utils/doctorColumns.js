beb2211bff83e06a7b6a77bc6707c651d87a045c
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-07-22 15:26:36 -0700

    Add files via upload

---
+++ Frontend/src/features/doctors/utils/doctorColumns.js
@@ -0,0 +1,25 @@
+/* File: src/features/doctors/utils/doctorColumns.js */ 
+
+export const doctorColumns = [
+    {
+        key: "first_name",
+        label: "First Name",
+    },
+    {
+        key: "last name",
+        label: "Last name",
+    },
+    {
+        key: "specialty",
+        label: "Specialty",
+    },
+    {
+        key: "email",
+        label: "Email",
+    },
+    {
+        key: "phone",
+        label: "Phone",
+    },
+];
+
