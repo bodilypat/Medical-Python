5ac91c16cb5351e00bbf1ec28dae9b1342553bf2
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-07-22 11:27:02 -0700

    Add files via upload

--- Frontend/src/features/patients/utils/patientColumns.js
+++ Frontend/src/features/patients/utils/patientColumns.js
@@ -1,1 +1,37 @@
-File: src/features/patients/utils/patientColumn.js 
+/* File: src/features/patients/utils/patientColumns.js */
+
+export const patientColumns = [
+    {
+        key: "first_name",
+        label: "First Name",
+    },
+    {
+        key: "last_name",
+        label: "Last Name",
+    },
+    {
+        key: "gender",
+        label: "Gender",
+    },
+    {
+        key: "date_of_birth",
+        label: "Date of Birth",
+    },
+    {
+        key: "email",
+        label: "Email",
+    },
+    {
+        key: "phone",
+        label: "Phone",
+    },
+    {
+        key: "blood_group",
+        label: "Blood Group",
+    },
+    {
+        key: "address",
+        label: "Address",
+    }
+];
+ 
