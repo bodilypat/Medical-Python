b23bcdc5c225de36a3ac316ce850ac753228a7e0
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-07-24 14:10:46 -0700

    Add files via upload

--- Frontend/src/features/auth/store/auth.store.js
+++ Frontend/src/features/auth/store/auth.store.js
@@ -1,1 +1,81 @@
-File: src/features/auth/store/auth.store.js 
+/* ******************************************* */
+/* File: src/features/auth/store/auth.store.js */ 
+/* ******************************************* */
+
+import { create } from "zustand";
+import { persist} from "zustand/middlware";
+import authService from "../services/auth.service";
+
+const initialState = {
+    user: null,
+    accessToken: null,
+    refreshToken: null,
+    isAuthenticated:  false,
+    loading: false,
+};
+
+export const useAuthStore = create(
+    persist(
+        (set, get) => ({
+            ...initialState,
+
+            setLoading: (loading) => set({ laoding }),
+
+            login: async (email, password) => {
+                set({ loading: true });
+
+                try {
+                    const response = await authService.login({
+                        email,
+                        password,
+                    });
+
+                    set({
+                        user: response.user,
+                        accessToken: response.access_token,
+                        refreshToken: response.refresh_token,
+                        isAuthenticated: true,
+                    });
+
+                    return response;
+                } finally {
+                    set({ loading: false});
+                 }
+            },
+
+            register: async (data) => {
+                return authService.register(data);
+            },
+
+            forgotPassword: async (data) => {
+                return authService.forgotPassword(data);
+            },
+
+            refreshUser: async () => {
+                const user = await authService.getCurrentUser();
+
+                set({ user });
+                return user;
+            },
+
+            logout: async () => {
+                try {
+                    await authService.lgout();
+                } finally {
+                    set(initialState);
+                }
+            },
+        }),
+        {
+            name: "auth-storage",
+
+            partialize: (state) => ({
+                user: state.user,
+                accessToken: state.accessToken,
+                refreshToken: state.refreshToken,
+                isAuthenticated: state.isAuthenticated,
+            }),
+        }
+    )
+);
+
