da652d7285db3152b94abde4c0eab7aae25607ff
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-07-31 12:16:07 -0700

    Add files via upload

--- Frontend/src/features/prescriptions/services/prescription.service.js
+++ Frontend/src/features/prescriptions/services/prescription.service.js
@@ -1,1 +1,66 @@
-File: src/features/prescription/services/perscription.service.js
+/* ***************************************************************** */
+/* File: src/features/prescriptions/services/prescription.service.js */ 
+/* ***************************************************************** */
+
+import api from "../../../services/api";
+
+const prescriptionService = {
+
+    getAll(params){
+
+        return api.get(
+            "/prescriptions",
+            {params}
+        );
+
+    },
+
+    getById(id){
+
+        return api.get(
+            `/prescriptions/${id}` 
+        );
+    },
+
+    create(data){
+
+        return api.post(
+            "/prescriptions",
+            data 
+        );
+    },
+
+    update(id, data){
+
+        return api.put(
+            `/prescriptions/${id}`,
+            data 
+        );
+    },
+
+    remove(id){
+
+        return api.delete(
+            `/prescriptions/${id}`
+        );
+    },
+
+    getPatientHistory(PatientId){
+
+        return api.get(
+            `/patients/${patientId}/prescriptions`
+        );
+    },
+
+    print(id){
+
+        return api.get(
+            `/prescriptions/${id}/print`,
+            {
+                responseType: "blob"
+            }
+        );
+    }
+};
+export default prescriptionService;
+
