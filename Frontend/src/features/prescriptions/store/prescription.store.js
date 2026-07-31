fbe8164d8fedd18cea5cb1c4cb9e3308b316872c
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-07-31 12:16:47 -0700

    Add files via upload

--- Frontend/src/features/prescriptions/store/prescription.store.js
+++ Frontend/src/features/prescriptions/store/prescription.store.js
@@ -1,1 +1,30 @@
-File: src/features/prescriptions/store/prescription.store.js
+/* ************************************************************ */
+/* File: src/features/prescriptions/store/prescription.store.js */ 
+/* ************************************************************ */
+import { create } from "zustand";
+
+export const usePrescriptionStore = create(
+    (set) => ({
+        prescriptions: [],
+        selectedPrescription: null,
+        medicines: [],
+        loading: false,
+        error: null,
+
+        setPrescriptions: (data) => 
+            set({
+                prescriptions:data 
+            }),
+
+        setSelectedPrescription: (data) =>
+            set({
+                selectedPrescription:data 
+            }),
+
+        setMedicines: (data) =>
+            set({
+                medicines:data 
+            })
+    })
+);
+
