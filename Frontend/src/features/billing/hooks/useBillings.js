235193822d3972b2b671002549039a419cc3b8f2
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-09-23 09:26:12 -0700

    Add useBillings hook for billing functionality

---
+++ Frontend/src/features/billing/hooks/useBillings.js
@@ -0,0 +1,6 @@
+/* *********************************************** */
+/* File: src/features/billing/hooks/useBillings.js */ 
+/* *********************************************** */
+
+import { useCallback, useEffect, useMemo, useState } from "react";
+import billingService from "../services/billingService";
